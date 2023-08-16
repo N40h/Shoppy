4aae8f247fb5fccf423d99526de981176bfb9665
Naoh <27915369+N40h@users.noreply.github.com>
2023-08-16 18:50:33 +0200

    Added Firebase database to the project

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 /***********************
+/*         Import
+/**********************/
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js';
+import {
+	getDatabase,
+	ref,
+	push,
+} from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
+
+/***********************
+/*         Settings
+/**********************/
+const appSettings = {
+	databaseURL:
+		'https://shopping-list-89412-default-rtdb.europe-west1.firebasedatabase.app/',
+};
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const itemsInDB = ref(database, 'items');
+
+/***********************
 /*         DOM
 /**********************/
 const inputField = document.getElementById('input__field');
@@ -10,5 +32,6 @@ const addButton = document.getElementById('add__button');
 addButton.addEventListener('click', () => {
 	let inputValue = inputField.value;
 
-	console.log(inputValue);
+	push(itemsInDB, inputValue);
+	console.log(`${inputValue} added to database`);
 });
